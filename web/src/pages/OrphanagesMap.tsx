f7d5e44f7a4cb60f4bd07ea27d856269147e971a
lucassantosf <lucas46em@gmail.com>
2021-02-04 16:31:08 -0300

    Day 3 at 21:53

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { MapContainer  , TileLayer, Marker, Popup } from 'react-leaflet';
+import { FiPlus , FiArrowLeftCircle} from 'react-icons/fi';
+import { MapContainer  , TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import logoImg from '../images/ico.jpg';
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: logoImg,
+  iconSize: [58,68],
+  iconAnchor: [29,68],
+  popupAnchor: [170,2]
+})
 
 function OrphanagesMap(){
   const position = [51.505, -0.09];
@@ -32,14 +40,15 @@ function OrphanagesMap(){
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-23.4772332,-47.5367404]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+        <Marker position={[-23.4772332,-47.5367403]} icon={mapIcon}>
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar dos dog's
+            <Link to="/orpghanages/1"><FiArrowLeftCircle size={20} color="#fff" /></Link>
           </Popup>
         </Marker>
       </MapContainer>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orpghanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
